18b9cef656df4433dce9af7d14ca26b4881cdfbb
Krish Sawant <krish.sawant@Krishs-MacBook-Air-2.local>
2026-05-09 23:20:20 +0530

    added chunker

---
+++ backend/ingestion/chunker.py
@@ -0,0 +1,35 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200,
+    separators=["\n\n", "\n", ". ", " ", ""],
+)
+
+
+
+def chunk_documents(documents: list):
+    """
+    Splits parsed documents into smaller chunks
+    while preserving metadata.
+    """
+
+    chunked_docs = []
+
+    for doc in documents:
+
+        text = doc["text"]
+        metadata = doc["metadata"]
+
+        chunks = text_splitter.split_text(text)
+
+        for chunk in chunks:
+            chunked_docs.append(
+                {
+                    "text": chunk,
+                    "metadata": metadata,
+                }
+            )
+
+    return chunked_docs
